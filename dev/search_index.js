b6cfb800c1e43eb9f1857baee2179ca953ac19e8
Documenter.jl <documenter@juliadocs.github.io>
2020-09-01 18:55:57 +0000

    build based on a0695b8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#LFAToolkit","page":"LFAToolkit","title":"LFAToolkit","text":"","category":"section"},{"location":"","page":"LFAToolkit","title":"LFAToolkit","text":"Local Fourier Analysis for arbitrary order finite element type operators","category":"page"},{"location":"","page":"LFAToolkit","title":"LFAToolkit","text":"Modules = [LFAToolkit]","category":"page"}]
+}

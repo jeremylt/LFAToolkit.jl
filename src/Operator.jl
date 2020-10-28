64a547ef88c80e25450fee1a5f445f2a96297f38
jeremylt <thompson.jeremy.luke@gmail.com>
2020-10-28 14:30:26 -0600

    operator - missed gallery in test code

--- src/Operator.jl
+++ src/Operator.jl
@@ -412,20 +412,7 @@ total = sum(v);
 ```jldoctest
 # setup
 mesh = Mesh2D(1.0, 1.0);
-basis = TensorH1LagrangeBasis(4, 4, 2);
-    
-function diffusionweakform(du::Array{Float64}, w::Array{Float64})
-    dv = du*w[1]
-    return [dv]
-end
-    
-# diffusion operator
-inputs = [
-    OperatorField(basis, [EvaluationMode.gradient]),
-    OperatorField(basis, [EvaluationMode.quadratureweights]),
-];
-outputs = [OperatorField(basis, [EvaluationMode.gradient])];
-diffusion = Operator(diffusionweakform, mesh, inputs, outputs);
+diffusion = GalleryOperator("diffusion", 4, 4, mesh);
     
 # element matrix computation
 # note: either syntax works

ac08cb647c327a01662583193eecbfadcf8f2d6f
jeremylt <thompson.jeremy.luke@gmail.com>
2021-02-11 16:47:57 -0700

    style

--- src/Basis.jl
+++ src/Basis.jl
@@ -847,7 +847,9 @@ function TensorMacroElementBasisFrom1D(
     # scale for element interfaces if prolongation basis
     if overlapquadraturepoints
         interpolation1dmacro =
-        Diagonal(1 ./ (interpolation1dmacro*ones(numbernodes1dmacro)))*interpolation1dmacro
+            Diagonal(
+                1 ./ (interpolation1dmacro*ones(numbernodes1dmacro)),
+            )*interpolation1dmacro
     end
 
     # use basic constructor

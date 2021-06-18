c94962b65563b7254fd320d61be23724f14ace76
jeremylt <thompson.jeremy.luke@gmail.com>
2021-06-18 10:45:23 -0600

    minor

--- papers/copper-mountain-2021/scripts/libceed_petsc_tests.sh
+++ papers/copper-mountain-2021/scripts/libceed_petsc_tests.sh
@@ -1,4 +1,4 @@
-# Echo command without running the command
+# echo command without running the command
 function quoted_echo()
 {
    local arg= string=
@@ -12,32 +12,37 @@ function quoted_echo()
    printf "%s\n" "${string# }"
 }
 
-# Run test over range of values
+# run test over range of values
 function run_tests()
 {
   local mpi_run=($PETSC_DIR/$PETSC_ARCH/bin/mpiexec -n 8)
   local common_args="-ksp_type richardson -ksp_monitor -dm_plex_box_lower -3,-3,-3 -dm_plex_box_upper 3,3,3 -smoothing chebyshev"
-  echo $common_args
+
   local max_p=6
   local max_v=4
   local num_dofs_1d=200
-  # Fine grid
+
+  # fine grid
   local fine_p=
   for ((fine_p = 2; fine_p <= max_p; fine_p++)); do
     echo
     echo "fine p:     " $fine_p
     local num_cells_1d=$((num_dofs_1d/(fine_p-1)+1))
-    # Coarse grid
+
+    # coarse grid
     local coarse_p=
     for ((coarse_p = 1; coarse_p < fine_p; coarse_p++)); do
       echo
       echo "  coarse p: " $coarse_p
-      # Smoothing passes
+
+      # smoothing passes
       local v=
       for ((v = 1; v <= max_v; v++)); do
         echo
         echo "    v:      " $v
         local all_args=("${common_args}" -degree $fine_p -coarse_degree $coarse_p -num_smooths $v -cells $num_cells_1d,$num_cells_1d,$num_cells_1d)
+
+        # run test
         echo 
         echo "Running test:"
         quoted_echo $mpi_run ./multigrid "${all_args[@]}"

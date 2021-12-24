82bc765430a1935bd378c19d4fee2f6f174686e5
Jeremy L Thompson <jeremy@jeremylt.org>
2021-12-24 10:51:43 -0700

    util - use symbolsoverrange util in more jupyter examples

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -101,11 +101,11 @@ end
 
 """
 ```julia
-computesymbols(operator, θ)
+computesymbols(preconditioner, ω, θ)
 ```
 
-Compute the eigenvalues and eigenvectors of the symbol matrix for an operator
-  over a range of θ from -π/2 to 3π/2
+Compute the eigenvalues and eigenvectors of the symbol matrix for a preconditioned
+  operator over a range of θ from -π/2 to 3π/2
 
 # Arguments:
 - `preconditioner`: Preconditioner to compute symbol matries for
@@ -190,6 +190,115 @@ function computesymbolsoverrange(
     for (step, θ) in enumerate(rangeiterator)
         θ = collect(θ)
         A = computesymbols(preconditioner, ω, θ)
+        currenteigen = eigen(A)
+        eigenvalues[step, :] = currenteigen.values
+        eigenvectors[step, :, :] = currenteigen.vectors
+        θ_range[step, :] = θ
+    end
+
+    # return
+    return (θ_range, eigenvalues, eigenvectors)
+end
+
+"""
+```julia
+computesymbols(multigrid, ω, v, θ)
+```
+
+Compute the eigenvalues and eigenvectors of the symbol matrix for a multigrid
+  preconditioned operator over a range of θ from -π/2 to 3π/2
+
+# Arguments:
+- `multigrid`:     Preconditioner to compute symbol matries for
+- `p`:             Smoothing parameter array
+- `v`:             Pre and post smooths iteration count array, 0 indicates no pre or post smoothing
+- `numbersteps1d`: Number of values of θ to sample in each dimension
+                     Note: numbersteps1d^dimension symbol matrices will be computed
+
+# Returns:
+- Values of θ sampled
+- Eigenvalues of symbol matrix at θ sampled
+- Eigenvectors of symbol matrix at θ sampled
+
+# Example:
+```jldoctest
+using LFAToolkit;
+
+numbersteps1d = 5
+
+using LinearAlgebra
+
+for dimension in 1:3
+    # setup
+    mesh = []
+    if dimension == 1
+        mesh = Mesh1D(1.0);
+    elseif dimension == 2
+        mesh = Mesh2D(1.0, 1.0);
+    elseif dimension == 3
+        mesh = Mesh3D(1.0, 1.0, 1.0);
+    end
+    ctofbasis = TensorH1LagrangeBasis(3, 5, 1, dimension, lagrangequadrature=true);
+
+    # operators
+    finediffusion = GalleryOperator("diffusion", 5, 5, mesh);
+    coarsediffusion = GalleryOperator("diffusion", 3, 5, mesh);
+
+    # smoother
+    jacobi = Jacobi(finediffusion);
+
+    # preconditioner
+    multigrid = PMultigrid(finediffusion, coarsediffusion, jacobi, [ctofbasis]);
+
+    # compute symbols
+    (_, eigenvalues, _) = computesymbolsoverrange(multigrid, [1.0], [1, 1], numbersteps1d);
+
+    # verify
+    eigenvalues = real(eigenvalues)
+    if dimension == 1
+       @assert maximum(eigenvalues[4, :]) ≈ 0.64
+    elseif dimension == 2
+       @assert maximum(eigenvalues[19, :]) ≈ 0.9082562365654528
+    elseif dimension == 3
+       @assert maximum(eigenvalues[94, :]) ≈ 1.4359882222222669
+    end
+end
+
+# output
+
+```
+"""
+function computesymbolsoverrange(
+    multigrid::Multigrid,
+    p::Array,
+    v::Array,
+    numbersteps1d::Int,
+)
+    # setup range
+    dimension = multigrid.fineoperator.dimension
+    numbersteps = numbersteps1d^dimension
+    θ_min = -π / 2
+    θ_max = 3π / 2
+    θ_range1d = LinRange(θ_min, θ_max, numbersteps1d)
+    θ_range = zeros(numbersteps, dimension)
+
+    # setup
+    numbermodes = size(multigrid.fineoperator.rowmodemap)[1]
+    eigenvalues = zeros(ComplexF64, numbersteps, numbermodes)
+    eigenvectors = zeros(ComplexF64, numbersteps, numbermodes, numbermodes)
+    rangeiterator = []
+    if dimension == 1
+        rangeiterator = θ_range1d
+    elseif dimension == 2
+        rangeiterator = Iterators.product(θ_range1d, θ_range1d)
+    elseif dimension == 3
+        rangeiterator = Iterators.product(θ_range1d, θ_range1d, θ_range1d)
+    end
+
+    # compute
+    for (step, θ) in enumerate(rangeiterator)
+        θ = collect(θ)
+        A = computesymbols(multigrid, p, v, θ)
         currenteigen = eigen(A)
         eigenvalues[step, :] = currenteigen.values
         eigenvectors[step, :, :] = currenteigen.vectors

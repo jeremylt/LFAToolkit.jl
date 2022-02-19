779a9a12a602f868189d84e42cd94a0cd6a21057
Jed Brown <jed@jedbrown.org>
2022-02-19 11:39:54 -0700

    Add Kosloff-Tal-Ezer and Hale-Trefethen strip transforms

--- examples/ex010_advection.jl
+++ examples/ex010_advection.jl
@@ -6,12 +6,34 @@ using LFAToolkit
 using LinearAlgebra
 using Plots
 
+function kosloff_tal_ezer(α)
+    g(s) = asin(α * s) / asin(α)
+    g_prime(s) = α / (asin(α) * sqrt(1 - (α * s)^2))
+    g, g_prime
+end
+
+function hale_trefethen_strip(ρ)
+    τ = π / log(ρ)
+    d = .5 + 1 / (exp(τ * π) + 1)
+    π2 = π / 2
+    # Unscaled functions of u
+    g_u(u) = log(1 + exp(-τ*(π2 + u))) - log(1 + exp(-τ*(π2-u))) + d*τ*u
+    g_prime_u(u) = 1 / (exp(τ*(π2+u)) + 1) + 1 / (exp(τ*(π2-u)) + 1) -d
+    # Normalizing factor and scaled functions of s
+    C = 1 / g_u(π/2)
+    g(s) = C * g_u(asin(s))
+    g_prime(s) = -τ*C/sqrt(1 - s^2) * g_prime_u(asin(s))
+    g, g_prime
+end
+
 # setup
 mesh = Mesh1D(1.0)
-P = 7;
+P = 11;
 Q = P;
 collocate = false
-mapping = sausage(9)
+#mapping = kosloff_tal_ezer(.98)
+#mapping = sausage(29)
+mapping = hale_trefethen_strip(1.4)
 basis = TensorH1LagrangeBasis(P, Q, 1, 1, collocatedquadrature = collocate, mapping = mapping)
 
 # associated phase speed
@@ -56,7 +78,7 @@ S = hcat(advection_symbol.(θ)...)'
 #min_S = minimum(S)
 #max_S = maximum(S)
 plot(θ / π, S ./ π)
-plot!(identity, label="exact", legend=:none, color=:black, ylims=(0, P-1))
+plot!(identity, label="exact", legend=:none, color=:black, ylims=(0, P+2))
 
 #tm = hcat(transformation_matrix.(θ)...)'
 #@show tm

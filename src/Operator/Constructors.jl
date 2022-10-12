9d7ec9ae15de4fb058f82abab84e28e0655e226b
AdelekeBankole <adeleke.bankole@colorado.edu>
2022-10-12 14:44:18 -0600

    fix style

--- src/Operator/Constructors.jl
+++ src/Operator/Constructors.jl
@@ -267,7 +267,7 @@ function GalleryOperator(
     numbernodes1d::Int,
     numberquadraturepoints1d::Int,
     mesh::Mesh;
-    parameters::Tuple{Float64, Float64},
+    parameters::Tuple{Float64,Float64},
     collocatedquadrature::Bool = false,
     mapping::Union{Tuple{Function,Function},Nothing} = nothing,
 )
